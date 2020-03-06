5bfc2dd32d049a99f48088c4f388fe14ed04a3e2
mkess-ivy <montier.kess@gmail.com>
2020-03-06 14:48:21 -0500

    testing all device and screen sizes

--- footer.php
+++ footer.php
@@ -18,29 +18,44 @@
 <div class="footer-area full">
 	<div class="main-page">
 		<footer id="colophon" class="site-footer inner">
-			<div class="bhm-wrapper">
+			<div class="bhm-wrapper footer-menu">
 				<div class="frow justify-between">
 					<div class="footer-logo"><a href="/"><img src="<?php echo get_field('bhm_footer_logo', 'option')['url'] ?>" /></a></div>
 					<div class="quick-links">
 						<div class="footer-menu-title">Quick Links</div>
-						<ul class="quick-links-menu">
-							<li><a href="#">Home</a></li>
-							<li><a href="#">About Us</a></li>
-							<li><a href="#">Services</a></li>
-							<li><a href="#">Contact Us</a></li>
-							<li><a href="#">Employee Portal</a></li>
-							<li><a href="#">FAQs</a></li>
-						</ul>
+						<nav class="footer-navigation">
+							<?php
+							wp_nav_menu( array(
+								'theme_location' => 'footer-menu',
+							) );
+							?>
+						</nav><!-- #site-navigation -->
 					</div>
 					<div class="contact">
 						<div class="footer-menu-title">Get in Touch</div>
-						<ul>
-							<li><span><img src="<?php echo get_field('address_icon', 'option')['url'] ?>" /></span>&nbsp;&nbsp;1404 West Baltimore Street <span class="city">Baltimore, Maryland 21223</span></li>
-							<li><span><img src="<?php echo get_field('phone_icon', 'option')['url'] ?>" /></span>&nbsp;&nbsp;410-800-2121 (office)</li>
-							<li>443-478-4716 (fax)</li>
-							<li>443-938-9944 (emergency)</li>
-							<li class="mail_footer"><span><img src="<?php echo get_field('mail_icon', 'option')['url'] ?>" /></span>&nbsp;&nbsp;<a href="mailto:admin@bhmhs.org">admin@bhmhs.org</a></li>
-						</ul>
+						<div class="git-items-wrapper">
+							<div class="item">
+								<span class="bullet-img"><img src="<?php echo get_field('address_icon', 'option')['url'] ?>" /></span>
+								<span class="bullet-text">1404 West Baltimore Street<span class="city">Baltimore, Maryland 21223</span>
+								</span>
+							</div>
+							<div class="item">
+								<span class="bullet-img"><img src="<?php echo get_field('phone_icon', 'option')['url'] ?>" /></span>
+								<span class="bullet-text">410-800-2121 (office)</span>
+							</div>
+							<div class="item">
+								<span class="no-bullet-img"></span>
+								<span class="bullet-text no-bullet">443-478-4716 (fax)</span>
+							</div>
+							<div class="item">
+								<span class="no-bullet-img"></span>
+								<span class="bullet-text no-bullet">443-938-9944 (emergency)</span>
+							</div>
+							<div class="item">
+								<span class="bullet-img"><img src="<?php echo get_field('mail_icon', 'option')['url'] ?>" /></span>
+								<span class="bullet-text mail-icon"><a href="mailto:admin@bhmhs.org">admin@bhmhs.org</a></span>
+							</div>
+						</div>
 					</div>
 					<div class="award-logo"><a href="/"><img src="<?php echo get_field('awards_logo', 'option')['url'] ?>" /></a></div>
 				</div>
--- functions.php
+++ functions.php
@@ -45,6 +45,7 @@ if ( ! function_exists( 'bhm_setup' ) ) :
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus( array(
 			'menu-1' => esc_html__( 'Primary', 'bhm' ),
+			'footer-menu' => __( 'Footer', 'bhm'),
 		) );
 
 		/*
